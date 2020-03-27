382421e02552940b652bc0fa8c3d22397a8b3cb4
Anandkumar0997 <62599053+Anandkumar0997@users.noreply.github.com>
2020-03-27 15:13:55 +0530

    Update OpencartRunner.java

--- OpenCart/src/test/java/com/TestRunner/OpencartRunner.java
+++ OpenCart/src/test/java/com/TestRunner/OpencartRunner.java
@@ -4,12 +4,12 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)//To run with cucumber class
 @CucumberOptions(
-		features = "src\\main\\resources\\Feature\\OpencartTestFile.feature",
-		plugin = {"pretty", "html:reports/cucumber-html-report","json:reports/cucumber-html-report/jsonreport","com.cucumber.listener.ExtentCucumberFormatter:reports/Extentreports/Extentreport.html"},
-		tags= {"@TC_01_Register,@TC_02_LoginDetails,@TC_03_InvalidDetails, @TC_04_InvalidEmail, @TC_05_DesktopDisplay,@TC_06_AddtoCart"},
-		glue = {"com.stepDefinition"},
+		features = "src\\main\\resources\\Feature\\OpencartTestFile.feature",//feature file location
+		plugin = {"pretty", "html:reports/cucumber-html-report","json:reports/cucumber-html-report/jsonreport","com.cucumber.listener.ExtentCucumberFormatter:reports/Extentreports/Extentreport.html"},//To generate reports
+		tags= {"@TC_01_Register,@TC_02_LoginDetails,@TC_03_InvalidDetails, @TC_04_InvalidEmail, @TC_05_DesktopDisplay,@TC_06_AddtoCart"},//To refer testcases
+		glue = {"com.stepDefinition"},//name of step definition
 		monochrome = true
 		)
 public class OpencartRunner {
